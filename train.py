c9203a8d014c4265e70ef1c2a0f643ccd8687fe7
Gabriel Metola <metolag@gmail.com>
2022-06-15 18:29:12 +0200

    v15.0 evolving LR and momentum

--- train.py
+++ train.py
@@ -552,32 +552,32 @@ def main(opt, callbacks=Callbacks()):
         meta = {'lr0': (1, 1e-5, 1e-1),  # initial learning rate (SGD=1E-2, Adam=1E-3)
                 'lrf': (1, 0.01, 1.0),  # final OneCycleLR learning rate (lr0 * lrf)
                 'momentum': (0.3, 0.6, 0.98),  # SGD momentum/Adam beta1
-                'weight_decay': (1, 0.0, 0.001),  # optimizer weight decay
-                'warmup_epochs': (1, 0.0, 5.0),  # warmup epochs (fractions ok)
-                'warmup_momentum': (1, 0.0, 0.95),  # warmup initial momentum
-                'warmup_bias_lr': (1, 0.0, 0.2),  # warmup initial bias lr
-                'box': (1, 0.02, 0.2),  # box loss gain
-                'cls': (1, 0.2, 4.0),  # cls loss gain
-                'cls_pw': (1, 0.5, 2.0),  # cls BCELoss positive_weight
-                'obj': (1, 0.2, 4.0),  # obj loss gain (scale with pixels)
-                'obj_pw': (1, 0.5, 2.0),  # obj BCELoss positive_weight
+                'weight_decay': (0, 0.0, 0.001),  # optimizer weight decay
+                'warmup_epochs': (0, 0.0, 5.0),  # warmup epochs (fractions ok)
+                'warmup_momentum': (0, 0.0, 0.95),  # warmup initial momentum
+                'warmup_bias_lr': (0, 0.0, 0.2),  # warmup initial bias lr
+                'box': (0, 0.02, 0.2),  # box loss gain
+                'cls': (0, 0.2, 4.0),  # cls loss gain
+                'cls_pw': (0, 0.5, 2.0),  # cls BCELoss positive_weight
+                'obj': (0, 0.2, 4.0),  # obj loss gain (scale with pixels)
+                'obj_pw': (0, 0.5, 2.0),  # obj BCELoss positive_weight
                 'iou_t': (0, 0.1, 0.7),  # IoU training threshold
-                'anchor_t': (1, 2.0, 8.0),  # anchor-multiple threshold
-                'anchors': (2, 2.0, 10.0),  # anchors per output grid (0 to ignore)
+                'anchor_t': (0, 2.0, 8.0),  # anchor-multiple threshold
+                'anchors': (0, 2.0, 10.0),  # anchors per output grid (0 to ignore)
                 'fl_gamma': (0, 0.0, 2.0),  # focal loss gamma (efficientDet default gamma=1.5)
-                'hsv_h': (1, 0.0, 0.1),  # image HSV-Hue augmentation (fraction)
-                'hsv_s': (1, 0.0, 0.9),  # image HSV-Saturation augmentation (fraction)
-                'hsv_v': (1, 0.0, 0.9),  # image HSV-Value augmentation (fraction)
-                'degrees': (1, 0.0, 45.0),  # image rotation (+/- deg)
-                'translate': (1, 0.0, 0.9),  # image translation (+/- fraction)
-                'scale': (1, 0.0, 0.9),  # image scale (+/- gain)
-                'shear': (1, 0.0, 10.0),  # image shear (+/- deg)
+                'hsv_h': (0, 0.0, 0.1),  # image HSV-Hue augmentation (fraction)
+                'hsv_s': (0, 0.0, 0.9),  # image HSV-Saturation augmentation (fraction)
+                'hsv_v': (0, 0.0, 0.9),  # image HSV-Value augmentation (fraction)
+                'degrees': (0, 0.0, 45.0),  # image rotation (+/- deg)
+                'translate': (0, 0.0, 0.9),  # image translation (+/- fraction)
+                'scale': (0, 0.0, 0.9),  # image scale (+/- gain)
+                'shear': (0, 0.0, 10.0),  # image shear (+/- deg)
                 'perspective': (0, 0.0, 0.001),  # image perspective (+/- fraction), range 0-0.001
-                'flipud': (1, 0.0, 1.0),  # image flip up-down (probability)
+                'flipud': (0, 0.0, 1.0),  # image flip up-down (probability)
                 'fliplr': (0, 0.0, 1.0),  # image flip left-right (probability)
-                'mosaic': (1, 0.0, 1.0),  # image mixup (probability)
-                'mixup': (1, 0.0, 1.0),  # image mixup (probability)
-                'copy_paste': (1, 0.0, 1.0)}  # segment copy-paste (probability)
+                'mosaic': (0, 0.0, 1.0),  # image mixup (probability)
+                'mixup': (0, 0.0, 1.0),  # image mixup (probability)
+                'copy_paste': (0, 0.0, 1.0)}  # segment copy-paste (probability)
 
         with open(opt.hyp, errors='ignore') as f:
             hyp = yaml.safe_load(f)  # load hyps dict
@@ -662,8 +662,8 @@ if __name__ == "__main__":
                         data='C:/datasets/crack_detector_train/data_train.yaml',
                         device='0',
                         entity=None,
-                        epochs=80,
-                        evolve=None,
+                        epochs=50,
+                        evolve=20,
                         exist_ok=False,
                         freeze=[0],
                         hyp=WindowsPath('data/hyps/hyp.scratch-low.yaml'),
@@ -672,7 +672,7 @@ if __name__ == "__main__":
                         label_smoothing=0.0,
                         local_rank=-1,
                         multi_scale=False,
-                        name='v14_model_9classes',
+                        name='v15_evolving',
                         noautoanchor=False,
                         nosave=False,
                         noval=False,
@@ -687,6 +687,6 @@ if __name__ == "__main__":
                         sync_bn=False,
                         upload_dataset=False,
                         weights='yolov5s.pt',
-                        workers=6)
+                        workers=7)
         
     main(opt)
